5b3c96eb0fb59e71d2cdbcb02b33d58e8e69384d
Alex Lunyov <isuntc@gmail.com>
2019-01-28 18:27:57 +0800

    Remove dist from repo

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { isColor } from './utils';
-declare const lerpColor: (start: string, end: string, percent: number) => string | undefined;
-export default lerpColor;
--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const isColor: (c: any) => boolean;
-export declare const parseColor: (color: string) => number[] | undefined;
-export declare const stringifyColor: (color: number[], target: string) => string;
