91f33ef367aa64a995ae7ef6af2a4d3846c4fbba
Alex Lunyov <isuntc@gmail.com>
2019-02-13 23:20:48 +0800

    Update demo

--- demo.js
+++ demo.js
@@ -1,11 +1,12 @@
 import lerp from './src/index';
 
 const steps = document.getElementById('steps');
-const color1 = document.getElementById('color1');
-const color2 = document.getElementById('color2');
+// const color1 = document.getElementById('color1');
+// const color2 = document.getElementById('color2');
 const color = document.getElementById('color');
 const percent = document.getElementById('percent');
 const link = document.querySelector('h1 a');
+const colors = Array.from(document.querySelectorAll('.color-input'));
 
 link.innerHTML = link.innerText
   .split('')
@@ -14,7 +15,7 @@ link.innerHTML = link.innerText
 
 const linkChars = Array.from(link.querySelectorAll('span'));
 
-const stepsCount = 6;
+const stepsCount = 13;
 const stepsDivs = [];
 for (let i = 0; i < stepsCount; i += 1) {
   const div = document.createElement('div');
@@ -23,25 +24,32 @@ for (let i = 0; i < stepsCount; i += 1) {
 }
 
 function refreshColors() {
-  const c1 = color1.value;
-  const c2 = color2.value;
+  const colorValues = colors.map(c => c.value);
 
   stepsDivs.forEach((step, i) => {
-    step.style.backgroundColor = lerp(c1, c2, i / (stepsCount - 1));
+    step.style.backgroundColor = lerp(...colorValues, i / (stepsCount - 1));
   });
-  color.style.backgroundColor = lerp(c1, c2, Number(percent.value) / 100);
+  color.style.backgroundColor = lerp(
+    ...colorValues,
+    Number(percent.value) / 100
+  );
 
   linkChars.forEach((char, i, chars) => {
-    char.style.color = lerp(c1, c2, i / (chars.length - 1));
+    char.style.color = lerp(...colorValues, i / (chars.length - 1));
   });
 
-  link.style.backgroundImage = `linear-gradient(to right, ${c1}, ${c2})`;
+  link.style.backgroundImage = `linear-gradient(to right, ${colorValues.join(
+    ', '
+  )})`;
 }
 
-color1.value = '#00aaaa';
-color2.value = '#fd8878';
+colors[0].value = '#41bbe9';
+colors[1].value = '#b710da';
+colors[2].value = '#fd8978';
 refreshColors();
-color1.addEventListener('change', refreshColors);
-color2.addEventListener('change', refreshColors);
+
+colors.forEach(colorInput => {
+  colorInput.addEventListener('change', refreshColors);
+});
 percent.addEventListener('change', refreshColors);
 percent.addEventListener('input', refreshColors);
